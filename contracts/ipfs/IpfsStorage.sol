e095007ae3377fd413c21a5d35c1ed2c49aae980
erkhabibullina <erkhabibullina@gmail.com>
2023-06-17 12:45:20 +0300

    edit ipfs contract

--- contracts/ipfs/IpfsStorage.sol
+++ contracts/ipfs/IpfsStorage.sol
@@ -1,11 +1,26 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract IpfsStorage {
+contract IpfsStorage is Ownable  {
 
-    string[] public cids;
+    uint256 private tax = 0;
 
-    function saveCID(string memory cid) public {
+    string[] private cids;
+
+    function saveCID(string calldata cid) public {
         cids.push(cid);
+    }
+
+    function getCIDs() public view returns (string[] memory) {
+        return cids;
+    }
+
+    function setTax(uint256 newTax) public onlyOwner {
+        tax = newTax;
+    }
+
+    function getTax()  public view returns (uint256) {
+        return tax;
     }
 }
