e2b9a2ed4e6bb00e325df9c6597e463c3cd6ff8d
erkhabibullina <erkhabibullina@gmail.com>
2023-06-18 14:01:09 +0300

    add main actions

--- contracts/YieldGranter.sol
+++ contracts/YieldGranter.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.8;
 
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-
+import "./interfaces/IGauge.sol";
 import "./interfaces/IRouter.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "./vault/YieldGranterVaultBase.sol";
 
 contract YieldGranter is YieldGranterVaultBase, ReentrancyGuard {
@@ -20,7 +20,15 @@ contract YieldGranter is YieldGranterVaultBase, ReentrancyGuard {
 
     uint256 private deadline;
 
-    mapping (address => uint256) public donatedAmount;
+    uint256 private donationsCount;
+
+    mapping(address => uint256) private totalDonatedAmount;
+
+    mapping(address => uint256) private userBalance;
+
+    mapping(address => address) private donations;
+
+    mapping(uint256 => address) private users;
 
     constructor(
         address _gauge,
@@ -37,19 +45,99 @@ contract YieldGranter is YieldGranterVaultBase, ReentrancyGuard {
         velo = IERC20(_velo);
     }
 
-    function depositProxy(uint256 amountA, uint256 amountB) external {
+    modifier claimAction() {
+        _claimAll();
+        _;
+    }
+
+    function getReserves() public view returns (uint256, uint256) {
+        return router.getReserves(address(token1), address(token2), true);
+    }
+
+    function depositProxy(
+        uint256 amountA,
+        uint256 amountB,
+        address toProject
+    ) public nonReentrant claimAction {
+        if (donations[msg.sender] == address(0)) {
+            users[donationsCount] = msg.sender;
+            donationsCount++;
+        } else {
+            require(userBalance[msg.sender] == 0,
+                "You have an active donation. Please withdraw your current deposit to create new donation");
+        }
+        donations[msg.sender] = toProject;
+
+        token1.approve(msg.sender, amountA * 2);
+        token2.approve(msg.sender, amountB * 2);
         token1.transferFrom(msg.sender, address(this), amountA);
         token2.transferFrom(msg.sender, address(this), amountB);
-        uint256 lpTokenAmount = addLiquidity(amountA, amountB);
-        uint256 shares = super.deposit(lpTokenAmount, address(this));
+
+        uint256 lpTokenAmount = _addLiquidity(amountA, amountB);
+        super.deposit(address(this), lpTokenAmount);
+
+        _mint(msg.sender, lpTokenAmount);
+        userBalance[msg.sender] += lpTokenAmount;
     }
 
-    function withdrawProxy(uint256 amount, uint256 amountAMin, uint256 amountBMin) external {
-        uint lpTokenAmount = super.withdraw(amount, address(this));
-        removeLiquidity(lpTokenAmount, amountAMin, amountBMin);
+    function withdrawProxy(
+        uint256 amount,
+        uint256 amountAMin,
+        uint256 amountBMin
+    ) public nonReentrant claimAction {
+        _withdrawProxy(amount, amountAMin, amountBMin);
     }
 
-    function addLiquidity(uint256 amountA, uint256 amountB) private returns (uint256) {
+    function _withdrawProxy(
+        uint256 amount,
+        uint256 amountAMin,
+        uint256 amountBMin
+    ) private {
+        approve(address(this), 1000000000000000000);
+        require(allowance(msg.sender, address(this)) >= amount);
+        require(userBalance[msg.sender] >= amount, "Insufficient balance");
+
+        super.withdraw(msg.sender, amount);
+        (uint256 amountA, uint256 amountB) = _removeLiquidity(amount, amountAMin, amountBMin);
+        token1.transfer(msg.sender, amountA);
+        token2.transfer(msg.sender, amountB);
+
+        _burn(msg.sender, amount);
+        userBalance[msg.sender] -= amount;
+    }
+
+    function claim() public nonReentrant {
+        _claimAll();
+    }
+
+    function getDonatedAmount(address project) public view returns (uint256) {
+        return totalDonatedAmount[project];
+    }
+
+    function _claim(address caller) private {
+        address project = donations[caller];
+        if (project == address(0)) return;
+        //        require(project != address(0), "There is no project to donate, address 0");
+
+        uint256 rewardAmount = getReward(velo);
+        if (rewardAmount <= 0) return;
+        //        require(rewardAmount > 0, "Reward is 0");
+        uint256 userReward = rewardAmount * 9 / 10;
+        uint256 projectReward = rewardAmount - userReward;
+
+        velo.approve(address(this), rewardAmount * 2);
+        velo.transferFrom(address(this), caller, userReward);
+        velo.transferFrom(address(this), project, projectReward);
+        totalDonatedAmount[project] += projectReward;
+    }
+
+    function _claimAll() private {
+        for (uint256 i = 0; i < donationsCount; i++) {
+            _claim(users[i]);
+        }
+    }
+
+    function _addLiquidity(uint256 amountA, uint256 amountB) private returns (uint256) {
         token1.approve(address(router), amountA * 2);
         token2.approve(address(router), amountB * 2);
         _updateDeadline();
@@ -67,7 +155,7 @@ contract YieldGranter is YieldGranterVaultBase, ReentrancyGuard {
         return lpToken.balanceOf(address(this));
     }
 
-    function removeLiquidity(
+    function _removeLiquidity(
         uint256 lpTokenAmount,
         uint256 amountAMin,
         uint256 amountBMin
--- contracts/vault/YieldGranterVaultBase.sol
+++ contracts/vault/YieldGranterVaultBase.sol
@@ -14,7 +14,8 @@ abstract contract YieldGranterVaultBase is ERC20 {
     IGauge private gauge;
 
     IERC20 private immutable _asset;
-    uint8 private immutable _underlyingDecimals;
+
+    uint256 internal constant MINIMUM_LIQUIDITY = 10 ** 3;
 
     // events
     event Deposit(address indexed sender, address indexed owner, uint256 assets, uint256 shares);
@@ -30,185 +31,53 @@ abstract contract YieldGranterVaultBase is ERC20 {
     constructor(
         address asset_,
         address _gauge
-    ) ERC20("YieldGranter", "YGT") {
-
-        (bool success, uint8 assetDecimals) = _tryGetAssetDecimals(IERC20(asset_));
-        _underlyingDecimals = success ? assetDecimals : 18;
+    ) ERC20("YieldGranter", "YGTT1") {
         _asset = IERC20(asset_);
         gauge = IGauge(_gauge);
-    }
-
-    function _tryGetAssetDecimals(IERC20 asset_) private view returns (bool, uint8) {
-        (bool success, bytes memory encodedDecimals) = address(asset_).staticcall(
-            abi.encodeWithSelector(IERC20Metadata.decimals.selector)
-        );
-
-        if (success && encodedDecimals.length >= 32) {
-            uint returnedDecimals = abi.decode(encodedDecimals, (uint256));
-            if (returnedDecimals <= type(uint8).max) {
-                return (true, uint8(returnedDecimals));
-            }
-        }
-        return (false, 0);
-    }
-
-    function asset() public view virtual returns (address) {
-        return address(_asset);
-    }
-
-    function totalAssets() public view virtual returns (uint) {
-        return _asset.balanceOf(address(this));
-    }
-
-    function convertToShares(uint assets) public view virtual returns (uint) {
-        return _convertToShares(assets, Math.Rounding.Down);
-    }
-
-    function convertToAssets(uint shares) public view virtual returns (uint) {
-        return _convertToAssets(shares, Math.Rounding.Down);
     }
 
     function maxDeposit(address) public view virtual returns (uint) {
         return type(uint256).max;
     }
 
-    function maxMint(address) public view virtual returns (uint) {
-        return type(uint256).max;
-    }
-
     function maxWithdraw(address owner) public view virtual returns (uint) {
-        console.log("balance of is ", balanceOf(owner));
-        return _convertToAssets(balanceOf(owner), Math.Rounding.Down);
-    }
-
-    function maxRedeem(address owner) public view virtual returns (uint) {
         return balanceOf(owner);
     }
 
-    function previewDeposit(uint assets) public view virtual returns (uint) {
-        return _convertToShares(assets, Math.Rounding.Down);
+    function deposit(address caller, uint256 assets) internal virtual {
+        require(assets <= maxDeposit(caller), "ERC4626: deposit more than max");
+
+        _deposit(caller, assets);
     }
 
-    function previewMint(uint shares) public view virtual returns (uint) {
-        return _convertToAssets(shares, Math.Rounding.Up);
+    function withdraw(address caller, uint256 assets) internal virtual {
+        require(assets <= maxWithdraw(caller));
+        _withdraw(caller, assets);
     }
 
-    function previewWithdraw(uint assets) public view virtual returns (uint) {
-        return _convertToShares(assets, Math.Rounding.Up);
-    }
-
-    function previewRedeem(uint shares) public view virtual returns (uint) {
-        return _convertToAssets(shares, Math.Rounding.Down);
-    }
-
-    function deposit(uint256 assets, address owner) public virtual returns (uint256) {
-        require(assets <= maxDeposit(owner), "ERC4626: deposit more than max");
-
-        console.log("deposit assets are", assets);
-        uint256 shares = previewDeposit(assets);
-        console.log("deposit shares are", shares);
-        _deposit(owner, assets, shares);
-
-        return shares;
-    }
-
-    function mint(uint shares, address owner) public virtual returns (uint) {
-        require(shares <= maxMint(owner));
-
-        uint assets = previewMint(shares);
-
-        _deposit(owner, assets, shares);
-
-        return assets;
-    }
-
-    function withdraw(
-        uint assets,
-        address owner
-    ) public virtual returns (uint) {
-        require(assets <= maxWithdraw(owner));
-
-        console.log("before shares");
-        uint shares = previewWithdraw(assets);
-        console.log("after shares", shares);
-
-        _withdraw(owner, assets, shares);
-
-        return assets;
-    }
-
-    function redeem(
-        uint shares,
-        address caller,
-        address owner
-    ) public virtual returns (uint) {
-        require(shares <= maxRedeem(owner));
-
-        uint assets = previewRedeem(shares);
-
-        _withdraw(owner, assets, shares);
-
-        return assets;
+    function getReward(IERC20 rewardToken) internal returns (uint256) {
+        address[] memory rewardTokens = new address[](1);
+        rewardTokens[0] = address(rewardToken);
+        gauge.getReward(address(this), rewardTokens);
+        return rewardToken.balanceOf(address(this));
     }
 
     function _deposit(
-        address owner,
-        uint assets,
-        uint shares
+        address caller,
+        uint assets
     ) internal virtual {
-        _asset.transferFrom(owner, address(this), assets);
-
-        _mint(owner, shares);
-
         _asset.approve(address(gauge), assets);
-        gauge.deposit(assets, 0);
+        gauge.depositAll(0);
 
-        emit Deposit(_msgSender(), owner, assets, shares);
+        emit Deposit(_msgSender(), caller, assets, assets);
     }
 
     function _withdraw(
-        address owner,
-        uint assets,
-        uint shares
+        address caller,
+        uint256 assets
     ) internal virtual {
-        console.log("before _burn and shares is ", shares);
-        _burn(owner, shares);
-        console.log("after _burn");
         gauge.withdrawToken(assets, 0);
-        console.log("after withdrawToken gauge");
 
-        _asset.transfer(owner, assets);
-        console.log("after _asset transfer");
-
-        emit Withdraw(_msgSender(), owner, owner, assets, shares);
-    }
-
-    function _convertToShares(
-        uint assets,
-        Math.Rounding rounding
-    ) internal view virtual returns (uint) {
-        console.log("convert assets are", assets);
-        console.log("convert totalSupply are", totalSupply() + 10 ** _decimalOffset());
-        console.log("convert totalAssets are", totalAssets() + 1);
-        return assets.mulDiv(
-            totalSupply() + 10 ** _decimalOffset(),
-            totalAssets() + 1,
-            rounding
-        );
-    }
-
-    function _convertToAssets(
-        uint shares,
-        Math.Rounding rounding
-    ) internal view virtual returns (uint) {
-        return shares.mulDiv(
-            totalAssets() + 1,
-            totalSupply() + 10 ** _decimalOffset(),
-            rounding
-        );
-    }
-
-    function _decimalOffset() internal view virtual returns (uint8) {
-        return 0;
+        emit Withdraw(_msgSender(), caller, caller, assets, assets);
     }
 }
